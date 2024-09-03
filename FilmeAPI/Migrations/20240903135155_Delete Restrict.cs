1593afc18dda7c48cb104c026c14e12f904eb0cf
Djhérondhy Pinheiro <djherondhyoficial@gmail.com>
2024-09-03 10:17:29 -0400

    feat✨: Deleção Restrita e RawQuerySQL em Cinema buscando ciname pelo EnderecoId

---
+++ FilmeAPI/Migrations/20240903135155_Delete Restrict.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmeAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteRestrict : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_cinemas_enderecos_EnderecoId",
+                table: "cinemas");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_cinemas_enderecos_EnderecoId",
+                table: "cinemas",
+                column: "EnderecoId",
+                principalTable: "enderecos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_cinemas_enderecos_EnderecoId",
+                table: "cinemas");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_cinemas_enderecos_EnderecoId",
+                table: "cinemas",
+                column: "EnderecoId",
+                principalTable: "enderecos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
